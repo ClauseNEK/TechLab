cd24a395c0b6aefefca464b41865ebe17688d501
holmganga <siggi.sejr@gmail.com>
2026-02-19 11:42:24 +0100

    opdateringer til main, ufærdige

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,43 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String[] args)
+    public static void main (String[] args) {
+        //initialiser scanner
+        Scanner input = new Scanner(System.in);
 
-    Scanner input = new Scanner(System.in);
+        //første besked til bruger
+        System.out.println("Hvor mange ting ville du låne?: ");
 
-    System.out.println("Hvor mange ting ville du låne?: ");
-    //(afvent) = scanner.nextLine();
+    }
+    public int getCount(){
+        Scanner input = new Scanner(System.in);
+        int count = 0;
+        count = input.nextInt();
+        return count;
+    }
 
+    public void loanItems(int count) {
+        for (int i = 0; i < count; i++) {
+            Scanner input = new Scanner(System.in);
 
-    // Items sektion:
+            System.out.println("\nUdlån #" + (i + 1));
+
+            System.out.print("\nType (bog/video/kit/computer): ");
+            String type = input.nextLine();
+
+            System.out.print("\nTitle: ");
+            String title = input.nextLine();
+
+            System.out.println("\nDage udlånt: ");
+            double currentLoanDays = input.nextDouble();
+
+            // kopieret metode i PetAdoptionCenter
+            if (type.equalsIgnoreCase("bog")) {
+                books[i] = new book(title);
+            } else if (anden type)
+
+        }
+    }
 
 
 }
