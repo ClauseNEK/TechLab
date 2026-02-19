516af73c202bbce8532e790030eef1cb13ceef90
ClauseNEK <nicl10000@stud.ek.dk>
2026-02-19 11:10:08 +0100

    Book.java

--- src/book.java
+++ src/book.java
@@ -1,2 +1,15 @@
 public class book {
+
+    private String author;
+
+    public book(int maxLoanDays, int currentLoanDays){
+        super(maxLoanDays, currentLoanDays);
+        this.author = author;
+
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Book's Author: " + author;
+    }
 }
