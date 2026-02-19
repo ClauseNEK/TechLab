5dc6402526ff51839c72eb38722b0d097e4e74a2
ClauseNEK <nicl10000@stud.ek.dk>
2026-02-19 10:34:39 +0100

    Tilføjelse: Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main (String[] args)
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Hvor mange ting ville du låne?: ");
+    //(afvent) = scanner.nextLine();
+
+
+
+    // Items sektion:
+
 
 }
