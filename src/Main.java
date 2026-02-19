1cb4ec7d928317645fff30fa78e6cc832584a5d4
holmganga <siggi.sejr@gmail.com>
2026-02-19 17:46:23 +0100

    items [array] metode flyttet fra main til items, hvor hver subclass skal override

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         int count = getCount(input);
 
         //laver en items[] array ved at kører loanItems
-        loanItems(count, input);
+        items.loanItems(count, input);
     }
 
     //tager imod antal udlån
@@ -23,37 +23,7 @@ public class Main {
         return count;
     }
 
-    public static items[] loanItems(int count, Scanner input) {
-        for (int i = 0; i < count; i++) {
-            items[] library = new items[count];
 
-            System.out.println("\nUdlån #" + (i + 1));
-
-            System.out.print("\nType (bog/video/kit/computer): ");
-            String type = input.nextLine();
-
-            System.out.print("\nTitle: ");
-            String title = input.nextLine();
-
-            System.out.println("\nDage udlånt: ");
-            double currentLoanDays = input.nextDouble();
-
-            // reference: PetAdoptionCenter
-            if (type.equalsIgnoreCase("bog")) {
-                library[i] = new book(title);
-            } else if (type.equalsIgnoreCase("video")) {
-                library[i] = new video(title);
-            } else if (type.equalsIgnoreCase("kit")) {
-                library[i] = new kit(title);
-            } else if (type.equalsIgnoreCase("computer")) {
-                library[i] = new computers(title);
-            } else {
-                System.out.println("Ukendt type.");
-                break;
-            }
-
-        }
-    }
 
 
 }
--- src/items.java
+++ src/items.java
@@ -10,20 +10,53 @@ public abstract class items {
     private double feePerDay;
 
     //constructor
-    public items (String title, String type, boolean isLoaned, double maxLoanDays, double currentLoanDays, double feePerDay){
+    public items (String title, String type, double currentLoanDays){
         this.title = title;
         this.type = type;
-        this.isLoaned = isLoaned;
-        this.maxLoanDays = maxLoanDays;
+        //this.isLoaned = isLoaned;
+        //this.maxLoanDays = maxLoanDays;
         this.currentLoanDays = currentLoanDays;
-        this.feePerDay = feePerDay;
+        //this.feePerDay = feePerDay;
     }
 
+    //metode til at lave array, der skal overrides i hver subclass baseret på hvilke attributes de hver især har
+    public static items[] loanItems(int count, Scanner input) {
+        for (int i = 0; i < count; i++) {
+            items[] library = new items[count];
+
+            System.out.println("\nUdlån #" + (i + 1));
+
+            System.out.print("\nType (bog/video/kit/computer): ");
+            String type = input.nextLine();
+
+            System.out.print("\nTitle: ");
+            String title = input.nextLine();
+
+            System.out.println("\nDage udlånt: ");
+            double currentLoanDays = input.nextDouble();
+
+            //library[] = new items(title, type, currentLoanDays);
+
+            // reference: PetAdoptionCenter
+//            if (type.equalsIgnoreCase("bog")) {
+//                library[i] = new book(title, author, currentLoanDays);
+//            } else if (type.equalsIgnoreCase("video")) {
+//                library[i] = new video(title);
+//            } else if (type.equalsIgnoreCase("kit")) {
+//                library[i] = new kit(title);
+//            } else if (type.equalsIgnoreCase("computer")) {
+//                library[i] = new computers(title);
+//            } else {
+//                System.out.println("Ukendt type.");
+//                break;
+//            }
+
+        }
+    }
 // getter til currentLoanDays - til book ihvertfald
     public double getCurrentLoanDays() {
         return currentLoanDays;
     }
-
 
 
     //metode der udregner late fee. returnere 0 hvis låne perioden ikke er overskredet
