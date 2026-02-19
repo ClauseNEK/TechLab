9be9d18b21cd10e7bdfe6c90c81ea362354c6f82
ClauseNEK <nicl10000@stud.ek.dk>
2026-02-19 11:41:23 +0100

    Book tilføjelser:

--- src/book.java
+++ src/book.java
@@ -1,17 +1,45 @@
-public class book {
+// title (Tages fra input scanneren)
+// type (Scanner)
+// Author (PRIVATE)
+// currentLoanDays (Scanner) - skal være en double eller int
+// book class (Opret book class med extends fra items)
+public abstract class book extends items {
 
     private String author;
 
-    public book(int maxLoanDays, int currentLoanDays){
-        super(maxLoanDays, currentLoanDays);
-        this.author = author;
 
-    }
+// Class Constructor
+public Book(int maxLoanDays, int currentLoanDays, String author){
+    super(maxLoanDays, currentLoanDays);
+    this.author = author;
+}
+
 
     @Override
-    public String getAuthor() {
-        return "Book's Author: " + author;
+    public double currentLoanDays() {
+        return super.currentLoanDays();
     }
+
+
+    @Override
+    public String title() {
+        return "Title: " + author;
+    }
+
+
+    // toString metode
+/*    @Override
+    public String toString() {
+        return "Product: " + name + " | Type: " + type + " ";
+    }
+*/
+
+
+
+
+
 }
+
+
 
 // BOOK
