5a0fc5a1abe2ee74992e2da64df066e8551f1c8a
holmganga <siggi.sejr@gmail.com>
2026-02-19 10:46:50 +0100

    item attributes and constructor added

--- src/items.java
+++ src/items.java
@@ -1,2 +1,19 @@
 public abstract class items {
+    //attributes
+    private String name;
+    private String type;
+    private boolean isLoaned;
+    private int maxLoanDays;
+    private int currentLoanDays;
+    private double feePerDay;
+
+    //constructor
+    public items (String name, String type, boolean isLoaned, int maxLoanDays, int currentLoanDays, double feePerDay){
+        this.name = name;
+        this.type = type;
+        this.isLoaned = isLoaned;
+        this.maxLoanDays = maxLoanDays;
+        this.currentLoanDays = currentLoanDays;
+        this.feePerDay = feePerDay;
+    }
 }
